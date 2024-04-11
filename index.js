131af0d17eb75e460e1fb538aeaec3955052f0f8
arensin8 <arensinani4@gmail.com>
2024-04-11 21:28:34 +0400

    The server and template configs

--- index.js
+++ index.js
@@ -7,11 +7,21 @@ const { AllRoutes } = require('./router/router');
 const {PORT} = process.env
 app.use(express.static('public'));
 app.use(express.json());
+app.use(expressEjsLayouts)
 app.use(express.urlencoded({extended : true}))
 app.set('view engine' , 'ejs')
 app.set('views' , 'views')
 app.set('layout' , './layouts/master')
 app.use(AllRoutes)
+
+app.use((req,res,next) => {
+    return res.status(404).json({
+        status : 404,
+        message : "Not found!"
+    })
+})
+
+
 app.use((err,req,res,next) => {
     return res.status(err.status || 500).json({
         status : err.status || 500,
