131f875fecf246bb3805b9d7230d2916c0b7bbf7
arensin8 <arensinani4@gmail.com>
2024-04-18 13:50:43 +0400

    blog controllers configs

--- controller/blog.controller.js
+++ controller/blog.controller.js
@@ -1,72 +1,105 @@
 const { elasticClient } = require("../config/elastic.config");
-const blogIndex = "blog"
-async function getALlBlogs(req,res,next){
-    try {
-        const value = req.params.value
-        const blogs = await elasticClient.search({
-            index: blogIndex,
-            q : value
+const blogIndex = "blog";
+async function getALlBlogs(req, res, next) {
+  try {
+    const value = req.params.value;
+    const blogs = await elasticClient.search({
+      index: blogIndex,
+      q: value,
+    });
+    return res.json(blogs.hits.hits);
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function createBlog(req, res, next) {
+  try {
+    const { title, author, text } = req.body;
+    const createResult = await elasticClient.index({
+      index: blogIndex,
+      document: {
+        title,
+        text,
+        author,
+      },
+    });
+    res.json(createResult);
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function removeBlog(req, res, next) {
+  try {
+    const { id } = req.params;
+    const deletedResut = await elasticClient.deleteByQuery({
+      index: blogIndex,
+      query: {
+        match: {
+          _id: id,
+        },
+      },
+    });
+    return res.json(deletedResut);
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function updateBlog(req, res, next) {
+  try {
+    const { id } = req.params;
+    const data = req.body;
+    Object.keys(data).forEach((key) => {
+      if (!data[key]) delete data[key];
+    });
+    const blog =
+      (
+        await elasticClient.search({
+          index: blogIndex,
+          query: { match: { _id: id } },
         })
-        return res.json(blogs.hits.hits)
-    } catch (error) {
-        next(error)
-    }
+      ).hits.hits?.[0] || {};
+    const payload = blog?._source || {};
+    const updateResut = await elasticClient.index({
+      index: blogIndex,
+      id,
+      body: { ...payload, ...data },
+    });
+    return res.json(updateResut);
+  } catch (error) {
+    next(error);
+  }
 }
 
-async function createBlog(req,res,next){
-    try {
-        const {title, author , text} = req.body;
-        const createResult = await elasticClient.index({
-            index : blogIndex,
-            document : {
-                title,
-                text,
-                author
-            }
-        })
-        res.json(createResult)
-    } catch (error) {
-        next(error)
-    }
+async function searchByTitle(req, res, next) {
+  try {
+  } catch (error) {
+    next(error);
+  }
 }
 
-async function removeBlog(req,res,next){
-    try {
-        
-    } catch (error) {
-        next(error)
-    }
+async function searchByMultiField(req, res, next) {
+  try {
+  } catch (error) {
+    next(error);
+  }
 }
 
-async function searchByTitle(req,res,next){
-    try {
-        
-    } catch (error) {
-        next(error)
-    }
-}
-
-async function searchByMultiField(req,res,next){
-    try {
-        
-    } catch (error) {
-        next(error)
-    }
-}
-
-async function searchByRegexp(req,res,next){
-    try {
-        
-    } catch (error) {
-        next(error)
-    }
+async function searchByRegexp(req, res, next) {
+  try {
+  } catch (error) {
+    next(error);
+  }
 }
 
 module.exports = {
-    getALlBlogs,
-    createBlog,
-    removeBlog,
-    searchByTitle,
-    searchByMultiField,
-    searchByRegexp
-}
+  getALlBlogs,
+  createBlog,
+  removeBlog,
+  searchByTitle,
+  searchByMultiField,
+  searchByRegexp,
+  updateBlog,
+};
--- router/blog.routes.js
+++ router/blog.routes.js
@@ -1,12 +1,17 @@
-const { createBlog, getALlBlogs } = require("../controller/blog.controller");
+const {
+  createBlog,
+  getALlBlogs,
+  removeBlog,
+  updateBlog,
+} = require("../controller/blog.controller");
 
-  
-  const BlogsRoutes = require("express").Router();
-  
-  BlogsRoutes.post("/create", createBlog);
-  BlogsRoutes.get("/list/:value?", getALlBlogs);
-  
-  module.exports = {
-    BlogsRoutes,
-  };
-  
+const BlogsRoutes = require("express").Router();
+
+BlogsRoutes.post("/create", createBlog);
+BlogsRoutes.get("/list/:value?", getALlBlogs);
+BlogsRoutes.delete("/delete/:id", removeBlog);
+BlogsRoutes.put("/update/:id", updateBlog);
+
+module.exports = {
+  BlogsRoutes,
+};
